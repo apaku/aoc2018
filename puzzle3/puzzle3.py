6976a1e51a286b4a155c6a91b245e73be93e70a4
Andreas Pakulat <andreas@froglogic.com>
2018-12-03 22:35:07 +0100

    Improve performance of the python version significantly
    Profiling showed that creating the unions is expensive, so instead let
    the various inner functions work as generators generating the points being
    overlapped. It appears the plain adding into a set is quite cheap. Brings
    the code from ~10 down to 1 second runtime.

--- puzzle3/puzzle3.py
+++ puzzle3/puzzle3.py
@@ -20,36 +20,33 @@ def overlaps(r1, r2):
         and r1['x2'] > r2['x1'] and r1['y2'] > r2['y1']
 
 def calculateoverlap(r1, r2):
-    overlappedrect = {
-        'x1': max(r1['x1'], r2['x1']),
-        'x2': min(r1['x2'], r2['x2']),
-        'y1': max(r1['y1'], r2['y1']),
-        'y2': min(r1['y2'], r2['y2'])
-    }
-    points = set()
-    for x in range(overlappedrect['x1'], overlappedrect['x2']):
-        for y in range(overlappedrect['y1'], overlappedrect['y2']):
-            points.add((x, y))
-    return points
+    x1 = max(r1['x1'], r2['x1'])
+    x2 = min(r1['x2'], r2['x2'])
+    y1 = max(r1['y1'], r2['y1'])
+    y2 = min(r1['y2'], r2['y2'])
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            yield (x,y)
 
 def overlap(r1, r2):
     if not overlaps(r1, r2):
         if overlaps(r2, r1):
             return calculateoverlap(r2, r1)
         else:
-            return set()
+            return []
     return calculateoverlap(r1, r2)
 
 def overlappedpoints(rect, others):
     allpoints = set()
     for other in others:
-        allpoints = allpoints.union(overlap(rect, other))
-    return allpoints
+        yield overlap(rect, other)
 
 def part2(rects):
-    allpoints = set()
+    seenpoints = set()
     for i in range(len(rects)):
-        allpoints = allpoints.union(overlappedpoints(rects[i], rects[i+1:]))
-    return len(allpoints)
+        for overlap in overlappedpoints(rects[i], rects[i + 1:]):
+            for point in overlap:
+                seenpoints.add(point)
+    return len(seenpoints)
 
-print("{}".format(part2(map(parse, sys.stdin.readlines()))))
+print("{}".format(part2(list(map(parse, sys.stdin.readlines())))))
