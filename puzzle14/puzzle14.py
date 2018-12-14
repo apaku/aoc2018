adfe91acdacd06c79308355976aa9df76891f225
Andreas Pakulat <andreas@froglogic.com>
2018-12-14 17:31:24 +0100

    Use lists instead of strings to speedup part2 a little
    Still comparatively slow (26 seconds), but already faster than string
    operations.

--- puzzle14/puzzle14.py
+++ puzzle14/puzzle14.py
@@ -6,9 +6,10 @@
 import itertools
 import sys
 
+
 def part1(maxlenstr):
     maxlen = int(maxlenstr)
-    recipelist = [3,7]
+    recipelist = [3, 7]
     elf1idx = 0
     elf2idx = 1
     while len(recipelist) < maxlen + 10:
@@ -18,32 +19,38 @@ def part1(maxlenstr):
         elf1idx = (elf1idx + 1 + elf1recipe) % len(recipelist)
         elf2idx = (elf2idx + 1 + elf2recipe) % len(recipelist)
         #print("{} {} {}".format(elf1idx, elf2idx, recipelist))
-    return recipelist[maxlen:maxlen+10]
+    return recipelist[maxlen:maxlen + 10]
+
 
 def part2(sublisttosearch):
-    recipelist = "37"
+    recipelist = [3, 7]
     elf1idx = 0
     elf2idx = 1
-    searchidx = 0
+    lensublisttosearch = len(sublisttosearch)
     while True:
-        elf1recipe = int(recipelist[elf1idx])
-        elf2recipe = int(recipelist[elf2idx])
-        newrecipes = str(elf1recipe + elf2recipe)
-        recipelist += newrecipes
-        elf1idx = (elf1idx + 1 + elf1recipe) % len(recipelist)
-        elf2idx = (elf2idx + 1 + elf2recipe) % len(recipelist)
-        if len(recipelist) < len(sublisttosearch):
+        elf1recipe = recipelist[elf1idx]
+        elf2recipe = recipelist[elf2idx]
+        newrecipe = elf1recipe + elf2recipe
+        if newrecipe > 9:
+            recipelist.append(1)
+        recipelist.append(newrecipe % 10)
+        lenrecipes = len(recipelist)
+        elf1idx = (elf1idx + 1 + elf1recipe) % lenrecipes
+        elf2idx = (elf2idx + 1 + elf2recipe) % lenrecipes
+        if lenrecipes < lensublisttosearch:
             continue
 
         #print("{} {} {}".format(elf1idx, elf2idx, recipelist))
-        idx = recipelist.find(sublisttosearch, searchidx)
-        if idx != -1:
-            return idx
-        searchidx = len(recipelist) - len(sublisttosearch)
+        if recipelist[-lensublisttosearch:] == sublisttosearch:
+            return lenrecipes - lensublisttosearch
+        if recipelist[-lensublisttosearch - 1:-1] == sublisttosearch:
+            return lenrecipes - lensublisttosearch - 1
+        if recipelist[-lensublisttosearch - 2:-2] == sublisttosearch:
+            return lenrecipes - lensublisttosearch - 2
     return None
+
 
 inputdata = sys.stdin.readline().strip()
 
 print("Part1: {}".format(''.join(map(str, part1(inputdata)))))
-print("Part2: {}".format(part2(inputdata)))
-
+print("Part2: {}".format(part2(list(map(int, inputdata)))))
