979fb204d85531536b7d61cb800c5a411e8ecf2f
Andreas Pakulat <andreas@froglogic.com>
2018-12-14 17:50:09 +0100

    Split up the loop body a bit for profiling
    Unfortunately I'm merely ending up at 'slicing is slow' and 'index
    calculation - with the modulo? - is slow'...

--- puzzle14/puzzle14.py
+++ puzzle14/puzzle14.py
@@ -22,35 +22,64 @@ def part1(maxlenstr):
     return recipelist[maxlen:maxlen + 10]
 
 
+def getrecipevals(recipes, idx1, idx2):
+    return (recipes[idx1], recipes[idx2])
+
+
+def getnewrecipe(recipe1, recipe2):
+    newrecipe = recipe1 + recipe2
+    if newrecipe > 9:
+        return [1, newrecipe % 10]
+    return [newrecipe % 10]
+
+
+def getnewidx(idx1, idx2, r1, r2, lenr):
+    return ((idx1 + 1 + r1) % lenr, (idx2 + 1 + r2) % lenr)
+
+
+def findsublist(l, lenl, sublist, lensublist):
+    def getslice(l, idx1, idx2):
+        return l[idx1:idx2]
+
+    if getslice(l, -lensublist, lenl) == sublist:
+        return lenl - lensublist
+    if getslice(l, -lensublist - 1, -1) == sublist:
+        return lenl - lensublist - 1
+    return None
+
+
 def part2(sublisttosearch):
     recipelist = [3, 7]
     elf1idx = 0
     elf2idx = 1
     lensublisttosearch = len(sublisttosearch)
     while True:
-        elf1recipe = recipelist[elf1idx]
-        elf2recipe = recipelist[elf2idx]
-        newrecipe = elf1recipe + elf2recipe
-        if newrecipe > 9:
-            recipelist.append(1)
-        recipelist.append(newrecipe % 10)
+        (elf1recipe, elf2recipe) = getrecipevals(recipelist, elf1idx, elf2idx)
+        newrecipedigits = getnewrecipe(elf1recipe, elf2recipe)
+        recipelist.extend(newrecipedigits)
         lenrecipes = len(recipelist)
-        elf1idx = (elf1idx + 1 + elf1recipe) % lenrecipes
-        elf2idx = (elf2idx + 1 + elf2recipe) % lenrecipes
+        (elf1idx, elf2idx) = getnewidx(elf1idx, elf2idx, elf1recipe,
+                                       elf2recipe, lenrecipes)
         if lenrecipes < lensublisttosearch:
             continue
 
         #print("{} {} {}".format(elf1idx, elf2idx, recipelist))
-        if recipelist[-lensublisttosearch:] == sublisttosearch:
-            return lenrecipes - lensublisttosearch
-        if recipelist[-lensublisttosearch - 1:-1] == sublisttosearch:
-            return lenrecipes - lensublisttosearch - 1
-        if recipelist[-lensublisttosearch - 2:-2] == sublisttosearch:
-            return lenrecipes - lensublisttosearch - 2
+        idx = findsublist(recipelist, lenrecipes, sublisttosearch,
+                          lensublisttosearch)
+        if idx is not None:
+            return idx
     return None
 
 
 inputdata = sys.stdin.readline().strip()
 
-print("Part1: {}".format(''.join(map(str, part1(inputdata)))))
+#print("Part1: {}".format(''.join(map(str, part1(inputdata)))))
+import cProfile
+import pstats
+p = cProfile.Profile()
+p.enable()
 print("Part2: {}".format(part2(list(map(int, inputdata)))))
+p.disable()
+s = pstats.Stats(p)
+s.sort_stats("tottime", "cumtime")
+s.print_stats()
